502396765ad3c6f2f61f3abab334de090b7db305
Mehraab Ferdouse <mehraabferdouse@gmail.com>
2025-09-30 18:08:24 +1000

    added group info to app_main.py

--- Assignment_3/app_main.py
+++ Assignment_3/app_main.py
@@ -1,3 +1,15 @@
+'''
+
+Group Name: DAN/EXT 28
+
+Group Members:
+FATEEN RAHMAN - s387983
+HENDRICK DANG (VAN HOI DANG)- s395598
+KEVIN ZHU (JIAWEI ZHU) - s387035
+MEHRAAB FERDOUSE - s393148
+
+'''
+
 from gui.views import App
 
 if __name__ == "__main__":
